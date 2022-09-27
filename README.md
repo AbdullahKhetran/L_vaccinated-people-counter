cdb0f49188ece123f7c6b85e346b5a9e4fec63e1
AbdullahKhetran <101284310+AbdullahKhetran@users.noreply.github.com>
2022-09-27 06:55:39 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# passenger-counter-app
-This app was created by me while learning JavaScript at Scrimba. It is a simple passenger counter app.
+This app was created by me while learning JavaScript at Scrimba.
+It is a simple passenger counter app.
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let entriesPara = document.getElementById("entries");
+let countEl = document.getElementById("count-el");
+let count = 0;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+    // console.log("increment button clicked");
+    count += 1;
+    countEl.textContent = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
+    // console.log("saved button clicked");
+    let countStr = count + " - ";
+    entriesPara.textContent += countStr;
     count = 0;
-    countEl.textContent = count
+    countEl.textContent = count;
+}
+
+function reset() {
+    // console.log("clear button clicked");
+    entriesPara.textContent = "Previous entries: ";
 }
