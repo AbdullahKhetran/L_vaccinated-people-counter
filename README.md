8e8fb389ffc5cd83b7f204c33e034fc33c0c849c
Abdullah Khetran <khetranabdullah@gmail.com>
2022-10-07 15:50:23 +0500

    modified index.html and index.js

---
+++ README.md
@@ -0,0 +1,1 @@
+This app was created by me while learning JavaScript at Scrimba. It is a simple app for counting vaccinated people. CSS styling has not been given much consideration since the aim was to use JS
---
+++ index.js
@@ -0,0 +1,22 @@
+let entriesPara = document.getElementById("entries");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+    console.log("increment button clicked");
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    console.log("saved button clicked");
+    let countStr = count + " - ";
+    entriesPara.textContent += countStr;
+    count = 0;
+    countEl.textContent = count;
+}
+
+function reset() {
+    console.log("clear button clicked");
+    entriesPara.textContent = "Previous entries: ";
+}
